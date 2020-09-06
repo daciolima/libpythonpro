d97ae473423563a0ed36c19a069a4ff8f58ef5fe
Dácio Lima <daciolima@MacBook-Pro.local>
2020-09-06 00:56:53 -0300

    Install lib pytest-mock, uso do mocker.patch para restaurar valor de metodo original

--- libpythonpro/tests/test_github_api.py
+++ libpythonpro/tests/test_github_api.py
@@ -5,17 +5,18 @@ from libpythonpro import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars2.githubusercontent.com/u/12838006?v=4'
     resp_mock.json.return_value = {
         'login': 'daciolima', 'id': 12838006,
         'avatar_url': url,
     }
-    get_original = github_api.requests.get
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
     github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    return url
+
 
 
 def test_buscar_avatar(avatar_url):

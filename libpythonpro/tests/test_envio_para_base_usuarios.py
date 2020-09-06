2a9a741beab60c6ad030558cd90ffae98686f836
Dácio Lima <daciolima@MacBook-Pro.local>
2020-09-06 00:46:57 -0300

    Code - Isolamento de Import, refatoracao do metodo requests.get e também criação de uam fixture

--- libpythonpro/tests/test_envio_para_base_usuarios.py
+++ libpythonpro/tests/test_envio_para_base_usuarios.py
@@ -2,7 +2,6 @@ from unittest.mock import Mock
 
 import pytest
 
-from libpythonpro.spam.enviador_email import Enviador
 from libpythonpro.spam.main import EnviadorSpam
 from libpythonpro.spam.modelos import Usuario
 
